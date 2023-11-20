46310600334f81edcbebac92cc7b34ead026daca
Nyan Naing Lin <kwienyan009@gmail.com>
2023-11-20 19:58:29 +0630

    Add new files

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    Number:4,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
